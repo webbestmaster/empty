229cc7c13d9fee549087a5a12c2f0c782a20eb4d
turovtsov <dturovtsov@marathonbet.ru>
2018-08-22 13:09:04 +0300

    update package.json and flow-typed

--- flow-typed/swiper.js
+++ flow-typed/swiper.js
@@ -4,11 +4,20 @@ type SwiperOptionsType = {|
     +direction?: 'horizontal' | 'vertical',
     +slidesPerView?: 'auto' | number,
     +freeMode?: boolean,
+    +loop?: boolean,
     +watchOverflow?: boolean,
     +scrollbar?: {|
-        +el: string // eslint-disable-line id-length
+        +el: string, // eslint-disable-line id-length
+        +hide?: boolean
     |},
-    +mousewheel?: boolean
+    +pagination?: {|
+        // eslint-disable-next-line id-length
+        +el: '.swiper-pagination'
+    |},
+    +mousewheel?: boolean,
+    +autoplay?: {|
+        +delay?: number
+    |}
 |};
 
 declare module 'swiper' {
