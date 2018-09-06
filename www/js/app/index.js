f4092a324aa3112c699d8887559086032ed8513e
webbestmaster <web.best.master@gmail.com>
2018-09-06 22:25:05 +0300

    refactor

--- www/js/app/index.js
+++ www/js/app/index.js
@@ -6,19 +6,30 @@ import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import BrowserRouter from 'react-router-dom/BrowserRouter';
 
+import ReduxStoreProvider from '../app-provider';
+
 import System from '../component/system';
 import Auth from '../component/auth';
 import Home from '../page/home';
 
 import routes from './routes';
 
-export default (): Array<Node> => [
-    <Auth key="auth"/>,
-    <System key="system">
-        <BrowserRouter>
-            <Switch key="switch">
-                <Route component={Home} path={routes.index} exact/>
-            </Switch>
-        </BrowserRouter>
-    </System>
-];
+export default function App(): Node {
+    return (
+        /* eslint-disable react/jsx-max-depth */
+        // you can replace the extra <div> with any react component
+        <ReduxStoreProvider>
+            <div>
+                <Auth key="auth"/>
+                <System key="system">
+                    <BrowserRouter>
+                        <Switch key="switch">
+                            <Route component={Home} path={routes.index} exact/>
+                        </Switch>
+                    </BrowserRouter>
+                </System>
+            </div>
+        </ReduxStoreProvider>
+        /* eslint-enable react/jsx-max-depth */
+    );
+}
--- www/js/index.js
+++ www/js/index.js
@@ -3,28 +3,6 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import {applyMiddleware, combineReducers, createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
-
 import App from './app';
 
-import * as reducers from './app-reducer';
-
-const reducer = combineReducers({
-    ...reducers
-});
-
-const composeEnhancers = composeWithDevTools({
-    // options like actionSanitizer, stateSanitizer
-});
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
-render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    window.document.querySelector('.js-app-wrapper')
-);
+render(<App/>, window.document.querySelector('.js-app-wrapper'));
