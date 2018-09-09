1d7fa46c80a0a1fffc97a63873f0355a37ba399b
turovtsov <dturovtsov@marathonbet.ru>
2018-09-09 11:58:19 +0300

    i18n

--- www/js/component/locale/translation/en-us/data.js
+++ www/js/component/locale/translation/en-us/data.js
@@ -6,11 +6,6 @@ export const enUs: LangDataType = {
     /* eslint-disable id-match, id-length */
     META__LANGUAGE_NAME: 'English',
 
-    HEADER__TOP_LINE: 'Try our new lotto games',
-    HEADER__BOTTOM_LINE: 'And win the jackpot',
-    HEADER__PLAY_NOW: 'Play now!',
-    GAME_CARD__PLAY: 'Play',
-
     LOGIN_POPUP__PLEASE_LOG_IN_OR_JOIN_NOW: 'Please log in or join now.',
     LOGIN_POPUP__INPUT_USERNAME: 'Username',
     LOGIN_POPUP__INPUT_PASSWORD: 'Password',
