913c0a6141f7f50e847c9f547f40e50bf54754b7
turovtsov <dturovtsov@marathonbet.ru>
2019-01-21 18:53:19 +0300

    ya - jewels-and-stones

---
+++ test/jewels-and-stones.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin
+});
+
+let lines = [];
+const timeStampName = 'duration';
+
+console.time(timeStampName);
+
+rl
+    .on('line', (line) => {
+        lines.push(line);
+    })
+    .on('close', () => {
+        const [jewels, stones] = lines;
+
+        let result = 0;
+
+        const stonesLength = stones.length;
+
+        for (let stoneIndex = 0; stoneIndex < stonesLength; stoneIndex += 1) {
+            if (jewels.includes(stones.charAt(stoneIndex))) {
+                result += 1;
+            }
+        }
+
+        process.stdout.write(result.toString());
+        process.stdout.write('\n');
+
+        console.timeEnd(timeStampName);
+    });
---
+++ test/readme.md
@@ -0,0 +1,5 @@
+## how to run, example
+
+```bash
+node ./jewels-and-stones.js < ./jewels-and-stones.txt
+```
