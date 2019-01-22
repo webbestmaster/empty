fcc968828ad7900965efa54d10ffcdfcf72c3466
turovtsov <dturovtsov@marathonbet.ru>
2019-01-22 19:07:54 +0300

    ya - merge

--- test/merge.js
+++ test/merge.js
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
 
 let isFirst = true;
 
-const result = [];
+let result = [];
 
 rl
     .on('line', line => {
@@ -15,14 +15,48 @@ rl
             return;
         }
 
-        const array = line.split(' ').map(a => parseInt(a, 10));
+        const array = line.split(' '); //.map(a => parseInt(a, 10));
 
-        array.shift();
+        // array.shift();
 
-        result.push(...array);
+        result = mergeSordedArrays(result, array, parseInt(array[0], 10) + 1);
     })
     .on('close', () => {
-        const sorted = result.sort((a, b) => a - b);
-
-        process.stdout.write(sorted.join(' '));
+        process.stdout.write(result.join(' '));
     });
+
+
+function mergeSordedArrays(arrFirst, arrSecond, arrSecondLength) {
+    const arrSort = [];
+    // let arrSortLength = 0;
+    let i = 0;
+    let j = 1; // ignore first element
+
+    // сравниваем два массива, поочередно сдвигая указатели
+    const arrFirstLength = arrFirst.length;
+    // const arrSecondLength = arrSecond.length;
+
+    while (i < arrFirstLength && j < arrSecondLength) {
+        arrSort.push(arrFirst[i] - arrSecond[j] < 0 ? arrFirst[i++] : arrSecond[j++]);
+        // arrSort[arrSortLength] = arrFirst[i] < arrSecond[j] ? arrFirst[i++] : arrSecond[j++];
+        // arrSortLength += 1;
+    }
+
+    if (i === arrFirstLength) {
+        return arrSort.concat(arrSecond.slice(j));
+    }
+
+    if (j === arrSecondLength) {
+        return arrSort.concat(arrFirst.slice(i));
+    }
+
+    return arrSort.concat(arrFirst.slice(i), arrSecond.slice(j));
+
+    // обрабатываем последний элемент при разной длине массивов
+    // и возвращаем один отсортированный массив
+    // return [
+    //     ...arrSort,
+    //     ...arrFirst.slice(i),
+    //     ...arrSecond.slice(j)
+    // ];
+}
