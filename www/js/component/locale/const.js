ec3feea3edfe4039a4230e75631499b498d32e80
turovtsov <dturovtsov@marathonbet.ru>
2019-05-04 13:58:08 +0300

    ncu

--- www/js/component/locale/const.js
+++ www/js/component/locale/const.js
@@ -10,23 +10,21 @@ import {ruRu} from './translation/ru-ru/data';
 import {zhCn} from './translation/zh-cn/data';
 import {zhTw} from './translation/zh-tw/data';
 
-export const localeNameReference: {[key: string]: LocaleNameType} = {
+export const localeNameReference: {+[key: string]: LocaleNameType} = {
     enUs: 'en-US',
     ruRu: 'ru-RU',
     zhCn: 'zh-CN',
     zhTw: 'zh-TW',
 };
 
-export const allLocales: {[key: LocaleNameType]: LangDataType} = {
+export const allLocales: {+[key: LocaleNameType]: LangDataType} = {
     [localeNameReference.enUs]: enUs,
     [localeNameReference.ruRu]: ruRu,
     [localeNameReference.zhCn]: zhCn,
     [localeNameReference.zhTw]: zhTw,
 };
 
-const localeNameList: Array<LocaleNameType> = Object.keys(localeNameReference).map(
-    (localeKey: string): LocaleNameType => localeNameReference[localeKey]
-);
+export const localeNameList: Array<LocaleNameType> = Object.keys(allLocales);
 
 export const localeConst = {
     action: {
@@ -43,5 +41,4 @@ export const localeConst = {
             localeName: PROJECT_ID + '-locale-name-v.1.0',
         },
     },
-    localeNameList,
 };
