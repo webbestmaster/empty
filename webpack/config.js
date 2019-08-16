b65f4cc74a38098c34edc50f1676a97e05c47ade
turovtsov <dturovtsov@marathonbet.ru>
2019-08-16 12:49:55 +0300

    refactor

--- webpack/config.js
+++ webpack/config.js
@@ -1,21 +1,19 @@
-/* global process, __dirname */
+/* global process */
 
 /* eslint no-process-env: 0, id-match: 0, optimize-regex/optimize-regex: 0 */
 
-const path = require('path');
+const modeDevelopmentName = 'development';
+const modeProductionName = 'production';
 
-const modeDevelopment = 'development';
-const modeProduction = 'production';
-
-const nodeEnvironment = process.env.NODE_ENV || modeDevelopment;
+const nodeEnvironment = process.env.NODE_ENV || modeDevelopmentName;
 
 // module.exports.NODE_ENV = process.env.NODE_ENV || DEVELOPMENT;
 module.exports.isBuildServer = process.env.IS_BUILD_SERVER === 'YES';
 
-module.exports.isDevelopment = nodeEnvironment === modeDevelopment;
-module.exports.isProduction = nodeEnvironment === modeProduction;
+module.exports.isDevelopment = nodeEnvironment === modeDevelopmentName;
+module.exports.isProduction = nodeEnvironment === modeProductionName;
 
-module.exports.cwd = path.join(__dirname, '/../');
+module.exports.cwd = process.cwd();
 
 module.exports.fileRegExp = /\.(png|jpg|jpeg|gif|svg|otf|ttf|woff2?)$/;
 
@@ -24,4 +22,4 @@ module.exports.pathToDist = '/dist';
 module.exports.pathToStaticFileFolder = '/static/';
 
 module.exports.webpackDevServerPort = 8080;
-module.exports.ssrServerPort = 8282;
+module.exports.ssrServerPort = 8081;
