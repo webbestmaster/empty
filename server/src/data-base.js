67e389292bca6c9f271655356b4c8b8fc2d2dcd3
WebBestMaster <web.best.master@gmail.com>
2019-10-05 20:01:37 +0300

    db - begin

--- server/src/data-base.js
+++ server/src/data-base.js
@@ -1,35 +1,34 @@
 // @flow
 
-import {MongoClient} from 'mongodb';
 import assert from 'assert';
+
+import {MongoClient} from 'mongodb';
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'myproject';
+const databaseName = 'myproject';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, (error: Error | null, client: {}) => {
     assert.equal(null, error);
 
-    console.log("Connected successfully to mongodb server");
+    console.log('Connected successfully to mongodb server');
 
-    const dataBase = client.db(dbName);
+    const dataBase = client.db(databaseName);
 
     const collection = dataBase.collection('documents');
     // Insert some documents
-    collection.insertMany([
-        {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
+
+    collection.insertMany([{a: 1}, {a: 2}, {a: 3}], function (error_, result) {
         // assert.equal(err, null);
         // assert.equal(3, result.result.n);
         // assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
+        console.log('Inserted 3 documents into the collection');
         client.close();
         // callback(result);
     });
-
 });
 
 export const smth = 5;
