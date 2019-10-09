ebdcd74d35bc9143e6e7347ac0f8d94cae7ebb7d
WebBestMaster <web.best.master@gmail.com>
2019-10-09 13:30:38 +0300

    refactor read index.html

--- server/src/static-files.js
+++ server/src/static-files.js
@@ -1,32 +1,23 @@
 // @flow
 
-import fileSystem from 'fs';
+import {promises as fsPromises} from 'fs';
 import path from 'path';
 
 import {pathToDist} from '../../webpack/config';
-import {isError, isString} from '../../www/js/lib/is';
 
 import {stringForReplace} from './config';
 
 let indexHtmlTemplate: string = stringForReplace;
 
-// $FlowFixMe
-fileSystem.readFile(path.join('.', pathToDist, 'index.html'), 'utf8', function readIndexHtmlCallBack(
-    error: Error | null,
-    data: mixed
-) {
-    if (isError(error)) {
-        console.error(`Can not read ${pathToDist}/index.html !!!`);
-        return;
-    }
-
-    if (isString(data)) {
-        indexHtmlTemplate = data;
-        return;
-    }
-
-    throw new Error('Can not read file as string!');
-});
+fsPromises
+    .readFile(path.join('.', pathToDist, 'index.html'), 'utf8')
+    .then((fileText: string): string => {
+        indexHtmlTemplate = fileText;
+        return fileText;
+    })
+    .catch((error: Error) => {
+        throw error;
+    });
 
 export function getIndexHtmlTemplate(): string {
     return indexHtmlTemplate;
